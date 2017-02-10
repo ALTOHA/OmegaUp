2996b76163332feb04816d3a5f68ebdd9aace032
ALTOHA17 (JEI) <altoha17@hotmail.com>
2017-02-09 22:14:38 -0600

    Add files via upload

---
+++ Mensaje de Amor.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+
+using namespace std;
+
+int main(){
+int a, b, c, i, j;
+scanf ("%d", &a);
+for (i=0; i<a; i++){
+	scanf ("%d", &b);
+	for (j=0; j<b; j++){
+		printf ("<3");
+	}
+	printf("\n");
+}
+
+return 0;
+}
+
