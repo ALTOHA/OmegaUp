4a33e4c689a74ed214d202241f42c86ac2176352
JEI <altoha17@hotmail.com>
2017-12-28 14:11:22 -0600

    Add files via upload

---
+++ DevTequila La Tarea de chonito.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner lectura = new Scanner(System.in);
+        int a = lectura.nextInt();
+        System.out.println(a*a);
+        System.out.println(a*a*a);    
+    }
+    
+}
